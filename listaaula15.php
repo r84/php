968b9889310fb1a56bc53ffe48ca72169c1a3931
Raphael de matos stanula <39820693+r84@users.noreply.github.com>
2021-10-15 19:45:39 -0300

    Create listaaula15.php
    lista relativa a aula 15 da matéria de introdução ao desenvolvimento web

---
+++ listaaula15.php
@@ -0,0 +1,18 @@
+ 
+1) Solicite a entrada de um número e descubra se um número digitado é 
+par ou ímpar.  
+2) Faça um algoritmo PHP que receba os valores A e B, imprima-os em 
+ordem crescente em relação aos seus valores. Exemplo, para A=5, B=4. 
+Você deve imprimir na tela: "4 5". 
+3) Faça um algoritmo em PHP onde verifica se o valor da variável A é 
+maior ou menor que o valor da variável B. A mensagem a ser impressa 
+deve ser “A maior que B” ou “A menor que B”. 
+4) Crie um algoritmo para calcular a média final de um aluno, para isso, 
+solicite a entrada de três notas e as insira em um array, por fim, calcule 
+a média geral. Caso a média seja maior ou igual a seis, exiba aprovado, 
+caso contrário, exiba reprovado. Exiba também a média final calculada. 
+Ex: N1 = 5 | N2 = 10 | N3 = 4 | MG = 6,33 [Aprovado] 
+5) Crie um algoritmo que pergunte ao usuário seu nome e sua idade. Em 
+seguida verifique se a idade é maior ou menor que 18, exiba da 
+seguinte forma: Fulano é maior de 18 e tem XX Anos ou Fulano não é 
+maior de 18 e tem XX Anos.
